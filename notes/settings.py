a586951d7479203c49dfeb6f22e3f976c7ff612f
Diyorbek Baxtiyorov <159403803+Diyorbek788@users.noreply.github.com>
2024-05-12 11:42:05 +0500

    first commit

--- notes/settings.py
+++ notes/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2q7l6ta(q1pv1%z5g63p(xmgq!&4weh(ep2&g!3@m=yc51%n@a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['64.23.138.194']
 
 
 # Application definition
